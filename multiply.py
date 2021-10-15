740a32285fa7b163218273804c52e5d1adb85995
RachelNgo <ngan.ngo01@sjsu.edu>
2021-10-14 17:44:10 -0700

    Update multiply.py
    add more conditions

--- multiply.py
+++ multiply.py
@@ -2,20 +2,20 @@
 
 def multiply_list(input):
     """
-
+    
     """
     #if there is no numbers in list return False
-    # if not input:
-    #     return False
+    if not input:
+        return False
         
     
     
     #if  in list contains string return False
-    # for x in input:
-    #     if type(x) == str:
-    #         return False
+    for x in input:
+        if type(x) == str:
+            return False
    
-    if input:
+    else:
         # start the product by 1 because 0 will return 0
          result = 1
          for x in list:
