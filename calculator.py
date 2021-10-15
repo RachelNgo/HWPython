bc1e680df7ee27c9023df9c81c80f05ab348a466
RachelNgo <ngan.ngo01@sjsu.edu>
2021-10-14 21:01:29 -0700

    Update calculator.py
    remove parse-input call func

--- calculator.py
+++ calculator.py
@@ -17,14 +17,13 @@ def calculator(number1, number2, operator):
         return number1 ** number2
     else:
         return False
- 
 def parse_input():
     """
     This function will take input from the user and call the calculator function.
     """
     userInput = input("Enter equation: ")
-    (num1, operations, num2) = userInput.split(" ")
+    num1, operations, num2 = userInput.split(" ")
     num1 = float(num1)
     num2 = float(num2)
     print(calculator(num1, num2, operations))
-parse_input()
+
