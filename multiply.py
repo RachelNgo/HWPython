32917831c7653d5a0f1e92b4e07f4a817f434f7d
RachelNgo <ngan.ngo01@sjsu.edu>
2021-10-14 17:42:57 -0700

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,10 +1,27 @@
-def multiply_list(input):
 
-    result = 1
-    for x in list:
-        if x == 0:
-            return False
-        else:
-            result = result * x
+
+def multiply_list(input):
+    """
+
+    """
+    #if there is no numbers in list return False
+    # if not input:
+    #     return False
+        
+    
+    
+    #if  in list contains string return False
+    # for x in input:
+    #     if type(x) == str:
+    #         return False
+   
+    if input:
+        # start the product by 1 because 0 will return 0
+         result = 1
+         for x in list:
+            if x == 0:
+                return False
+            else:
+                result = result * x
 
     return result
